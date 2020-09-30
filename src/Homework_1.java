4945a05e15d26d39c968eb174421a7a8592b3d39
sssLAKIsss <slakis210195@gmail.com>
2020-09-30 19:01:06 +0300

    обновил комментарии

--- src/Homework_1.java
+++ src/Homework_1.java
@@ -7,10 +7,14 @@ import java.util.Stack;
 
 
 public class Homework_1 {
-    private static void CheckOn(char brk1, char brk2, Stack<Character> stack) { // Проверяем, расставлены ли конкретные скобки
-        for (int i = 1; i < stack.size(); i++) {                                // по математическим законам,если да, то
-            if (stack.get(i) == brk2) {                                         // затираем их на пробел
-                if (stack.get(i - 1) == brk1) {
+    /** Проверяем, расставлены ли конкретные скобки
+     * * по математическим законам,если да, то
+     * затираем их на пробел
+     */
+    private static void CheckOn(char brk1, char brk2, Stack<Character> stack) {
+        for (int i = 1; i < stack.size(); i++) {
+            if (stack.get(i) == brk2) {
+                if (stack.get(i - 1) == brk1)
                     stack.set(i, ' ');
                     stack.set(i - 1, ' ');
                 } else if (stack.get(i - 1) == ' ') {
@@ -26,49 +30,71 @@ public class Homework_1 {
                 }
             }
         }
+
+    /**
+     * Cоздаем стек из последовательности вводимых
+     * с клавиатуры скобок
+     */
+    private static Stack<Character> createStack() throws IOException {
+        Stack<Character> stack = new Stack<>();
+
+            System.out.println("Введите последовательность скобок");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String str = reader.readLine();
+
+            for (int i = 0; i < str.length(); i++) {
+                stack.push(str.charAt(i));
+            }
+            return stack;
+
     }
 
-    private static Stack<Character> createStack() throws IOException {  // Cоздаем стек из последовательности вводимых с
-        Stack<Character> stack = new Stack<>();                         // с клавиатуры скобок
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String str = reader.readLine();
-
-        for (int i = 0; i < str.length(); i++) {
-            stack.push(str.charAt(i));
-        }
-        return stack;
-    }
-
-    private static void removeAndCheck(Stack<Character> stack) { // Удаление пустых значений до первого непустого, если в стеке ничего
-        for (int i = stack.size() - 1; i > -1 ; i--) {           // не останется, то последовательность скобок соответсвтует
-            if (stack.get(i) == ' ') {                           // условию задачи
+    /**
+     * Удаление пустых значений до первого непустого
+     * если в стеке ничего не останется, то последовательность скобок соответсвтует условию задачи
+     */
+    private static void removeAndCheck(Stack<Character> stack) {
+        for (int i = stack.size() - 1; i > -1 ; i--) {
+            if (stack.get(i) == ' ') {
                 stack.pop();
             }
             else break;
         }
 
         if (stack.size() != 0) {
-            System.out.println("Расставление скобок не по математике");
+            System.out.println("Расставление скобок не по правилам математики");
         }
-        else System.out.println("Расставление скобок  по математике");
+        else System.out.println("Расставление скобок  по правилам математики");
     }
 
-    public static void main(String[] args) throws IOException {
-        Stack<Character> stack = createStack();
+    /**
+     * Объявляет тип скобок, проходящих проверку
+     * Проверяет вашу последовательность методами, объявленными выше
+     */
+    public static  void finalCheck() {
+        try {
+            Stack<Character> stack = createStack();
 
-        char fig1 = '{';
-        char fig2 = '}';
-        char circl1 = '(';
-        char circl2 = ')';
-        char squad1 = '[';
-        char squad2 = ']';
+            char fig1 = '{';
+            char fig2 = '}';
+            char circl1 = '(';
+            char circl2 = ')';
+            char squad1 = '[';
+            char squad2 = ']';
 
-        CheckOn(squad1, squad2, stack);
-        CheckOn(circl1, circl2, stack);
-        CheckOn(fig1, fig2, stack);
+            CheckOn(squad1, squad2, stack);
+            CheckOn(circl1, circl2, stack);
+            CheckOn(fig1, fig2, stack);
 
-        removeAndCheck(stack);
-        //System.out.println(stack);
+            removeAndCheck(stack);
+        }
+        catch (IOException e) {
+            System.out.println("Ошибка ввода-вывода");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        finalCheck();
     }
 }
